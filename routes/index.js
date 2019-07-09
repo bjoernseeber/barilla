c4d7bf8aeb52e6566cddb5a65229eb9c1e78e3ec
Devan Pellow <devanpellow@gmail.com>
2019-07-09 17:49:37 +0200

    search feature returns multiple recipes using ingredients from mongo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,22 @@ const Recipe = require("../models/Recipe");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-	res.render("index");
+	// res.render("index");
+
+	Recipe.find({})
+	.then(recipe => {
+		const arrIngr = recipe.map(x=>x.ingredientsList)		
+		const arrIngrFlat = [].concat.apply([], arrIngr)
+		const objIngr = new Set(arrIngrFlat);
+		const arrIngrNoDup = [...objIngr]
+		console.log("hello", arrIngrNoDup);
+		res.render('index',{ arrIngrNoDup })
+
+	})
+	.catch(err => {
+		console.log("Error while updating the book: ", err);
+	});
+	
 });
 
 router.get("/recipes", (req, res) => {
@@ -19,18 +34,24 @@ router.get("/recipes", (req, res) => {
 
 router.post("/ingredients", (req, res) => {
 	const search = req.body.userIngredientInput;
-	Recipe.findMany({ ingredientsList: search })
+	Recipe.find({ ingredientsList: { $all: search } })
 		.then(recipe => {
-			console.log(recipe)
+			
+			console.log(recipe);
 		})
 		.catch(err => {
-			console.log("Error while updating the book: ", err);
+			console.log("Error while updating the recipe: ", err);
 		});
 	console.log(search);
 });
 
-// router.get('/ingredients', (req, res) => {
-// 	res.render('ingredients')
-// })
+router.get("/", (req, res, next) => {
+	const resultsArr = [];
+	Recipe.find({}).then(recipe => {
+		recipe.forEach(doc, err => {
+			resultsArr.push(doc);
+		});
+	});
+});
 
 module.exports = router;
