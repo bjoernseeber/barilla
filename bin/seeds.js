2eca7994b7124c71cad7b8faa78770b15f88a7da
Bjoern Seeber <bjoernseeber@yahoo.de>
2019-07-12 10:35:57 +0200

    seeds

--- bin/seeds.js
+++ bin/seeds.js
@@ -10,17 +10,6 @@ const Recipe = require("../models/Recipe");
 const bcryptSalt = 10;
 
 mongoose
-<<<<<<< HEAD
-	.connect("mongodb://localhost/barilla", { useNewUrlParser: true })
-	.then(x => {
-		console.log(
-			`Connected to Mongo! Database name: "${x.connections[0].name}"`
-		);
-	})
-	.catch(err => {
-		console.error("Error connecting to mongo", err);
-	});
-=======
   .connect(process.env.MONGODB_URI ||'mongodb://localhost/barilla', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
@@ -28,7 +17,6 @@ mongoose
   .catch(err => {
     console.error('Error connecting to mongo', err)
   });
->>>>>>> 0b0b06616de6f9791e32e66e4913fdcda988a089
 
 let users = [
 	{
